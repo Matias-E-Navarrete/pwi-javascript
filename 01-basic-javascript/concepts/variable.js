aa1d281f3cfb9b612d99299ccfd18d2c133eec05
Matias-E-Navarrete <matias.navarreteutn@gmail.com>
2022-04-21 20:25:00 -0300

    feat: javascript concepts

---
+++ 01-basic-javascript/concepts/variable.js
@@ -0,0 +1,17 @@
+
+// Variables: tipos de variables
+
+// * const: No se puede modificar
+const name = 'Pedro'
+const age = 19
+const hasGreater = true
+// * let: Alcance reducido, es mas seguro que usar var y si se puede modificar
+let name1 = 'Juan'
+let edad1 = 9
+let hasGreater1 = false
+let hobbies = ['Jugar Futbol', 'Tocar la guitarra', 'Comer']
+name1 = 'Romina'
+
+// * var: Alcace global (se puede usar en toda la aplicacion) y también se puede modificar. (NO ES RECOMENDADO)
+var name2 = 'Nicolas'
+name2 = 'Luca'
