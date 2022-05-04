88033da8965e584b5c4f08e3ebf504145f93e79a
Matias-E-Navarrete <matias.navarreteutn@gmail.com>
2022-05-04 08:09:21 -0300

    feat: javascript

--- 04-basic-javascript-selectores/js/login.js
+++ 04-basic-javascript-selectores/js/login.js
@@ -5,9 +5,28 @@
  */
 
 const loginButtons = document.getElementsByClassName('login-button') // Retorna un array con todos los elementos encontrados con la clase solicitada
-
 const loginButton = loginButtons[0]
 const blockAccountButton = loginButtons[1]
+const rememberCheck = document.getElementById('rememberCheck')
+const loginBtn = document.getElementById('login')
 
-console.log('loginButton', loginButton)
+console.log(rememberCheck)
+
+const loginForm = document.getElementById('cardLogin')
+
+
+
+loginButton.addEventListener('click', function () {
+    loginForm.classList.toggle('d-none')
+
+})
+
+rememberCheck.addEventListener('check', function () {
+    const isDisabled = loginBtn.hasAttribute('disabled')
+    if (isDisabled) {
+        loginBtn.removeAttribute('disabled')
+    } else {
+        loginBtn.setAttribute('disabled', 'true')
+    }
+})
 
