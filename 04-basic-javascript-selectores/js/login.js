2278bca54e2ff81138ecf7cc90cea764dbe5d261
Matias-E-Navarrete <matias.navarreteutn@gmail.com>
2022-05-03 21:00:10 -0300

    feat: login form

---
+++ 04-basic-javascript-selectores/js/login.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * Logica de login
+ *  
+ */
+
+const loginButtons = document.getElementsByClassName('login-button') // Retorna un array con todos los elementos encontrados con la clase solicitada
+
+const loginButton = loginButtons[0]
+const blockAccountButton = loginButtons[1]
+
+console.log('loginButton', loginButton)
+
